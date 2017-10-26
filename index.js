5f1de3dc67e7b7b11a6fd4754fba20bc838ce44e
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2017-10-26 13:57:36 +0700

    Log Be focused app price :cat2:

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const puppeteer = require('puppeteer')
 let browser
 
+const TRACKED_APP_URL = 'https://itunes.apple.com/us/app/be-focused-pro-focus-timer-goal-tracker/id961632517'
+
 const main = async () => {
   browser = await puppeteer.launch()
   const page = await browser.newPage()
-  await page.goto('https://google.com')
-  await page.screenshot({ path: 'google.png' })
+  await page.goto(TRACKED_APP_URL)
+  const price = await page.$eval(`div.price`, div => div.innerHTML)
+  console.log(price)
   await browser.close()
   console.log('finished!!!')
 }
